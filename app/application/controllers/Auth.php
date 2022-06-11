de1f06655a18d98be8286b7af2683f193a1f8519
@simpanaja12 <simpanaja12@gmail.com>
2022-06-11 21:15:55 +0700

    aaaaa

--- app/application/controllers/Auth.php
+++ app/application/controllers/Auth.php
@@ -11,7 +11,47 @@ class Auth extends CI_Controller
 
    public function index()
    {
-      $this->load->view('auth/login');
+      $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+      $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+      if ($this->form_validation->run() == false) {
+         $data['title'] = 'Login';
+         $this->load->view('auth/login');
+      } else {
+         $this->_login();
+      }
+   }
+
+   private function _login()
+   {
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+
+      $user = $this->db->get_where('user', ['email' => $email])->row_array();
+      //jika usernya ada
+      if ($user) {
+         //jika usernya aktif
+         if ($user['is_active'] == 1) {
+            //cek password
+            if (password_verify($password, $user['password'])) {
+               $data = [
+                  'email' => $user['email'],
+                  'role_id' => $user['role_id']
+               ];
+               $this->session->set_userdata($data);
+               redirect('user');
+            } else {
+               $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Wrong password! </div>');
+               redirect('auth/');
+            }
+         } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> This email is not activated! </div>');
+            redirect('auth/');
+         }
+      } else {
+         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered! </div>');
+         redirect('auth/');
+      }
    }
 
    public function register()
@@ -21,7 +61,7 @@ class Auth extends CI_Controller
       $this->form_validation->set_rules('pass1', 'Password', 'required|trim|matches[pass2]|min_length[8]', ['matches' => 'Password tidak sesuai', 'min_length' => 'Password kurang dari 8 karakter']);
       $this->form_validation->set_rules('pass2', 'Password', 'required|trim|matches[pass2]|min_length[8]', ['matches' => 'Password tidak sesuai', 'min_length' => 'Password kurang dari 8 karakter']);
       if ($this->form_validation->run() == false) {
-         $this->load->view('login/');
+         $this->load->view('auth/register');
       } else {
          $data = [
             'name' => htmlspecialchars($this->input->post('name', true)),
