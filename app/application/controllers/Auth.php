acb9f8416ff6015a708c0ba7f6718277785da555
@simpanaja12 <simpanaja12@gmail.com>
2022-06-11 20:31:06 +0700

    restore file

--- app/application/controllers/Auth.php
+++ app/application/controllers/Auth.php
@@ -3,9 +3,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Auth extends CI_Controller
 {
-
    public function index()
    {
-      $this->load->view('welcome_message');
+      $this->load->view('auth/login');
    }
 }
