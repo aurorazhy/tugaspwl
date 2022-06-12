ac0c6ddf722e83bfef1332e11453474884b88803
@simpanaja12 <simpanaja12@gmail.com>
2022-06-12 15:46:08 +0700

    update login

--- app/application/controllers/Auth.php
+++ app/application/controllers/Auth.php
@@ -11,7 +11,46 @@ class Auth extends CI_Controller
 
    public function index()
    {
-      $this->load->view('auth/register');
+      $this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email');
+      $this->form_validation->set_rules('password', 'Password', 'trim|required');
+
+      if ($this->form_validation->run() == false) {
+         $this->load->view('auth/login');
+      } else {
+         $this->_login();
+      }
+   }
+
+   private function _login()
+   {
+      $email = $this->input->post('email');
+      $password = $this->input->post('password');
+
+      $user = $this->db->get_where('user', ['email' => $email])->row_array();
+      //jika usernya ada
+      if ($user) {
+         //jika usernya aktif
+         if ($user['is_active'] == 1) {
+            //cek password
+            if (password_verify($password, $user['password'])) {
+               $data = [
+                  'email' => $user['email'],
+                  'role_id' => $user['role_id']
+               ];
+               $this->session->set_userdata($data);
+               redirect('user');
+            } else {
+               $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Wrong password! </div>');
+               redirect('auth/');
+            }
+         } else {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> This email is not activated! </div>');
+            redirect('auth/');
+         }
+      } else {
+         $this->session->set_flashdata('message', '<div class="alert alert-danger" role="alert"> Email is not registered! </div>');
+         redirect('auth/');
+      }
    }
 
    public function register()
@@ -32,7 +71,7 @@ class Auth extends CI_Controller
 
          $this->db->insert('user', $data);
          $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert"> Akun berhasil dibuat </div>');
-         redirect('auth/login');
+         redirect('auth/');
       }
    }
 
--- app/application/views/auth/register.php
+++ app/application/views/auth/register.php
@@ -2,23 +2,23 @@
 <html lang="en">
 
 <head>
-	<meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Register</title>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Register</title>
 </head>
 
 <body>
-	<form method="post" action="<?= base_url('auth/register/'); ?>">
-		<h1>Create Account</h1>
-		<input type="text" name="name" placeholder="Name"><br>
-		<input type="text" name="email" placeholder="Email"><br>
-		<input type="password" name="pass1" placeholder="Password">
-		<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
-		<input type="password" name="pass2" placeholder="Confirm Password">
-		<?= form_error('pass2', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
-		<button type="submit">Register</button><br>
-		<a href="<?= base_url('auth/kelogin/'); ?>">Login here</a>
+      <form method="post" action="<?= base_url('auth/register/'); ?>">
+            <h1>Create Account</h1>
+            <input type="text" name="name" placeholder="Name"><br>
+            <input type="text" name="email" placeholder="Email"><br>
+            <input type="password" name="pass1" placeholder="Password">
+            <?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
+            <input type="password" name="pass2" placeholder="Confirm Password">
+            <?= form_error('pass2', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
+            <button type="submit">Register</button><br>
+            <a href="<?= base_url(''); ?>">Login here</a>
 </body>
 
 </html>
