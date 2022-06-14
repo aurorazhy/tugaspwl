3d2700c7d08f58b05b79cf977fedef0aad924fb5
@simpanaja12 <simpanaja12@gmail.com>
2022-06-14 21:29:14 +0700

    waa

--- app/application/views/auth/register.php
+++ app/application/views/auth/register.php
@@ -1,3 +1,4 @@
+<div class="container-fluid py-5 bg-appbg"></div>
 <div class="container-fluid py-5 bg-appbg">
 	<div class="row">
 		<div class="col-6 offset-3 bg-light">
@@ -27,7 +28,7 @@
 								<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?>
 							</div>
 							<center>
-								<button type="submit" class="btn btn-appregis">Submit</button><br>
+								<button type="submit" class="btn btn-appregis text-light">Submit</button><br>
 								Sudah punya akun?<a class="btn btn-white" href="<?= base_url(''); ?>"> Masuk disini</a>
 							</center>
 						</form>
@@ -37,21 +38,5 @@
 			</div>
 		</div>
 	</div>
-<<<<<<< Updated upstream
 </div>
-
-
-
-<form method="post" action="<?= base_url('auth/register/'); ?>">
-	<h1>Create Account</h1>
-	<input type="text" name="name" placeholder="Name"><br>
-	<input type="text" name="email" placeholder="Email"><br>
-	<input type="password" name="pass1" placeholder="Password">
-	<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
-	<input type="password" name="pass2" placeholder="Confirm Password">
-	<?= form_error('pass2', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
-	<button type="submit">Register</button><br>
-	<a href=">Login here</a>
-=======
-</div>
->>>>>>> Stashed changes
+<div class="container-fluid py-5 bg-appbg"></div>
