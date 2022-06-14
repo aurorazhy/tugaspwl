0847ece6990d4a5ac590a964e178eb2cba21a3bf
Zhyraa <roralolxd@gmail.com>
2022-06-14 21:04:15 +0700

    design regis

--- app/application/views/auth/register.php
+++ app/application/views/auth/register.php
@@ -12,28 +12,27 @@
 						<form class="my-5" method="post" action="<?= base_url('auth/register/'); ?>">
 							<div class="form-group">
 								<label for="nama">Nama</label>
-								<input type="text" class="form-control" id="nama">
+								<input type="text" name="name" class="form-control" id="nama">
 							</div>
 							<div class="form-group">
 								<label for="email">Email</label>
-								<input type="email" class="form-control" id="email" aria-describedby="emailHelp">
-								<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								<input type="email" name="email" class="form-control" id="email" aria-describedby="emailHelp">
 							</div>
 							<div class="form-group">
 								<label for="pass1">Password</label>
-								<div class="d-flex flex-row mb-3">
-									<input type="password" class="form-control mr-4" id="pass1"><br>
-									<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
-									<input type="password" class="form-control" id="pass2"><br>
-									<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
+								<div class="d-flex flex-row">
+									<input type="password" name="pass1" class="form-control mr-4" id="pass1">
+									<input type="password" name="pass2" class="form-control" id="pass2">
 								</div>
+								<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?>
 							</div>
 							<center>
 								<button type="submit" class="btn btn-appregis">Submit</button><br>
-								Sudah punya akun?<a href="<?= base_url(''); ?>">Masuk disini</a>
+								Sudah punya akun?<a class="btn btn-white" href="<?= base_url(''); ?>"> Masuk disini</a>
 							</center>
 						</form>
 					</div>
+
 				</div>
 			</div>
 		</div>
@@ -51,4 +50,4 @@
 	<input type="password" name="pass2" placeholder="Confirm Password">
 	<?= form_error('pass2', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
 	<button type="submit">Register</button><br>
-	<a href="<?= base_url(''); ?>">Login here</a>
+	<a href=">Login here</a>
