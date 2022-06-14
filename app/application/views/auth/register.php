f2676be5814250a611829c4e08203394bd47c536
Zhyraa <roralolxd@gmail.com>
2022-06-14 20:17:53 +0700

    register design

--- app/application/views/auth/register.php
+++ app/application/views/auth/register.php
@@ -1,3 +1,41 @@
+<div class="container-md">
+	<div class="col-md-6 offset-3 my-5">
+		<div class="panel panel-default">
+			<div class="panel-head">
+				<center>
+					<h1>Buat Akun</h1>
+				</center>
+			</div>
+			<div class="panel-body">
+				<form class="my-5" method="post" action="<?= base_url('auth/register/'); ?>">
+					<div class="form-group">
+						<label for="nama">Nama</label>
+						<input type="text" class="form-control" id="nama">
+					</div>
+					<div class="form-group">
+						<label for="email">Email</label>
+						<input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+					</div>
+					<div class="form-group">
+						<label for="pass1">Password</label>
+						<div class="d-flex flex-row mb-3">
+							<input type="password" class="form-control mr-4" id="pass1"><br>
+							<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
+							<input type="password" class="form-control" id="pass2"><br>
+							<?= form_error('pass1', '<small class="text-danger pl-3"> ', '</small>'); ?><br>
+						</div>
+					</div>
+					<center>
+						<button type="submit" class="btn btn-primary">Submit</button><br>
+						Sudah punya akun?<a href="<?= base_url(''); ?>">Masuk disini</a>
+					</center>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
 <form method="post" action="<?= base_url('auth/register/'); ?>">
 	<h1>Create Account</h1>
 	<input type="text" name="name" placeholder="Name"><br>
