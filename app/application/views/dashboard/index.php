8484ca6db686b95fce13a68b64383e8b34ee746f
@simpanaja12 <simpanaja12@gmail.com>
2022-06-17 05:53:30 +0700

    Update index.php

--- app/application/views/dashboard/index.php
+++ app/application/views/dashboard/index.php
@@ -20,55 +20,68 @@
             </div>
             <!-- /.container-fluid -->
             <div class="container-fluid">
-               <div class="d-flex flex-row">
-                  <div class="">
-                     <div class="d-flex flex-row">
-                        <div class="card rounded-bottom p-0 ml-4" style="width: 25rem;">
-                           <div class="card-body text-white bg-appside">
-                              <div class="huge pr-2 text-right">0</div>
-                              <h5 class="card-title">Jumlah Barang</h5>
+               <div class="container">
+                  <div class="row">
+                     <div class="col-6 ml-4">
+                        <div class="row">
+                           <div class="col-6">
+                              <div class="card-body text-white bg-appside">
+                                 <div class="huge pr-2 text-right">0</div>
+                                 <p class="card-title text-right">Jumlah Barang</p>
+                              </div>
+                              <div class="card-body bg-appbg rounded-bottom">
+                                 <div class="d-flex">
+                                    <a href="<?= base_url() . 'dashboard/stokbarang'; ?>" class="text-appside" style="text-decoration: none;">Lihat Detail</a>
+                                    <div class="ml-auto"><i class="fas fa-arrow-right"></i></div>
+                                 </div>
+                              </div>
                            </div>
-                           <div class="card-body bg-appbg rounded-bottom">
-                              <div class="d-flex">
-                                 <a href="<?= base_url() . 'dashboard/stokbarang'; ?>" class="text-appside" style="text-decoration: none; font-size: 1.2rem;">Lihat Detail</a>
-                                 <div class="ml-auto p-2"> <i class="fa-solid fa-arrow-right-long">icon</i></div>
+
+                           <div class="col-6">
+                              <div class="card-body text-white bg-appside">
+                                 <div class="huge pr-2 text-right">0</div>
+                                 <p class="card-title text-right">Jumlah Transaksi</p>
+                              </div>
+                              <div class="card-body bg-appbg rounded-bottom">
+                                 <div class="d-flex">
+                                    <a href="<?= base_url() . 'dashboard/transaksi'; ?>" class="text-appside" style="text-decoration: none;">Lihat Detail</a>
+                                    <div class="ml-auto"><i class="fas fa-arrow-right"></i></div>
+                                 </div>
                               </div>
                            </div>
                         </div>
-                        <div class="card rounded-bottom p-0 ml-4" style="width: 25rem;">
-                           <div class="card-body text-white bg-appside">
-                              <div class="huge pr-2 text-right">0</div>
-                              <h5 class="card-title">Jumlah Transaksi</h5>
-                           </div>
-                           <div class="card-body bg-appbg rounded-bottom">
-                              <div class="d-flex">
-                                 <a href="<?= base_url() . 'dashboard/transaksi'; ?>" class="text-appside" style="text-decoration: none; font-size: 1.2rem;">Lihat Detail</a>
-                                 <div class="ml-auto p-2"> <i class="fa-solid fa-arrow-right-long">icon</i></div>
+
+                        <div class="row">
+                           <div class="col-12">
+                              <div class="card rounded-bottom my-4 d-flex flex-row bg-appside">
+                                 <img class="card-img-top" src="<?= base_url() . 'assets/img/profile/default.jpg'; ?>" style="height: 15rem; width: 15rem;" alt="Card image cap">
+                                 <div class="card-body">
+                                    <h5 class="card-title"><?= $user['name']; ?></h5>
+                                    <p class="card-text"><?= $user['email']; ?></p>
+                                    <p class="card-text">jadi member</p>
+                                 </div>
                               </div>
                            </div>
                         </div>
                      </div>
+                     <!--akhir kolom 1-->
 
-                     <div class="card rounded-bottom p-0 ml-4 my-4 d-flex flex-row bg-appside">
-                        <img class="card-img-top" src="<?= base_url() . 'assets/img/profile/default.jpg'; ?>" style="height: 15rem; width: 15rem;" alt="Card image cap">
-                        <div class="card-body">
-                           <h5 class="card-title"><?= $user['name']; ?></h5>
-                           <p class="card-text"><?= $user['email']; ?></p>
-                           <p class="card-text">jadi member</p>
+                     <div class="col-5 ml-5">
+                        <div class="card rounded-bottom">
+                           <div class="card-header bg-appside text-center">
+                              Catatan
+                           </div>
+                           <div class="card-body">
+                              <textarea name="" id="" cols="10" rows="10"></textarea>
+                           </div>
                         </div>
-                     </div>
-                  </div>
-
-                  <div class="card rounded-bottom p-0 mx-4 mb-4">
-                     <div class="card-header bg-appside text-center">
-                        Catatan
-                     </div>
-                     <div class="card-body">
-                        <textarea name="" id="" cols="70" rows="10"></textarea>
                      </div>
                   </div>
                </div>
             </div>
+            <!--akhir container-->
+
+
 
             </div>
             </div>
