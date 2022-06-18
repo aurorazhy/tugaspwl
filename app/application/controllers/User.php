653acc21e8076ef8d1cae4cd4e8753e5266db729
Zhyraa <roralolxd@gmail.com>
2022-06-18 18:44:43 +0700

    fungsi edit

--- app/application/controllers/User.php
+++ app/application/controllers/User.php
@@ -15,7 +15,7 @@ class User extends CI_Controller
       $this->load->view('templates/footer');
    }
 
-   public function stokbarang()
+   public function stokbarang($id = "")
    {
       $data['title'] = 'Stok Barang';
       $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
@@ -24,7 +24,15 @@ class User extends CI_Controller
       $this->load->view('templates/header', $data);
       $this->load->view('templates/sidebar', $data);
       $this->load->view('templates/topbar', $data);
+      if ($id == '') {
       $this->load->view('dashboard/sub_barang/form_add');
+      } else {
+         $where = array(
+            'id_barang' => $id
+         );
+         $data['barang'] = $this->M_barang->edit_data($where, 'barang')->result();
+         $this->load->view('dashboard/sub_barang/form_edit', $data);
+      }
       $this->load->view('dashboard/sub_barang/table', $data);
       $this->load->view('templates/footer');
    }
@@ -48,6 +56,35 @@ class User extends CI_Controller
             'harga_jual' => $harga_jual
          );
          $this->M_barang->insert_data($data, 'barang');
+         redirect(base_url() . 'user/stokbarang');
+      } else {
+         redirect(base_url() . 'user/stokbarang');
+      }
+   }
+
+   public function barang_edit_act()
+   {
+      $id = $this->input->post('id');
+      $this->form_validation->set_rules('nm', 'Nama Barang', 'required');
+      $this->form_validation->set_rules('qty', 'Quantity', 'required');
+
+      if ($this->form_validation->run() != false) {
+         $nama_barang = $this->input->post('nm');
+         $harga_beli = $this->input->post('hb');
+         $qty = $this->input->post('qty');
+         $harga_jual = $this->input->post('hj');
+
+         $where = array(
+            'id_barang' => $id
+         );
+
+         $data = array(
+            'nama_barang' => $nama_barang,
+            'harga_beli' => $harga_beli,
+            'qty' => $qty,
+            'harga_jual' => $harga_jual
+         );
+         $this->M_barang->update_data($where, $data, 'barang');
          redirect(base_url() . 'user/stokbarang');
       } else {
          redirect(base_url() . 'user/stokbarang');
---
+++ app/application/views/dashboard/sub_barang/form_edit.php
@@ -0,0 +1,44 @@
+<div class="contrainer-fluid">
+   <div class="container px-5 xy-2">
+      <div class="row">
+         <!-- Topbar Search -->
+         <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+               <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+               <div class="input-group-append">
+                  <button class="btn btn-primary" type="button">
+                     <i class="fas fa-search fa-sm"></i>
+                  </button>
+               </div>
+            </div>
+         </form>
+      </div>
+   </div>
+   <div class="row">
+      <div class="col-3 offset-1 py-3">
+         <?php foreach ($barang as $b) { ?>
+            <form action="<?= base_url() . 'user/barang_edit_act' ?>" method="post">
+               <div class="form-group">
+                  <label> Nama Barang </label>
+                  <input type="hidden" name="id" value="<?= $b->id_barang  ?>">
+                  <input type="text" name="nm" class="form-control" value="<?= $b->nama_barang  ?>">
+                  <?= form_error('nm'); ?>
+               </div>
+               <div class="form-group">
+                  <label> Harga Beli </label>
+                  <input type="number" name="hb" class="form-control" value="<?= $b->harga_beli ?>">
+               </div>
+               <div class="form-group">
+                  <label> Harga Jual </label>
+                  <input type="number" name="hj" class="form-control" value="<?= $b->harga_jual  ?>">
+               </div>
+               <div class="form-group">
+                  <label> Qty </label>
+                  <input type="number" name="qty" class="form-control" value="<?= $b->qty  ?>">
+                  <?= form_error('qty'); ?>
+               </div>
+               <div class="form-group">
+                  <input type="submit" value="Update" class="btn btn-primary">
+               </div>
+            </form><?php } ?>
+      </div>
