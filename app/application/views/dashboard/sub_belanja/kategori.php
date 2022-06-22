a5d874bbb99877cf87d9d2ff90f127ab8315d0b2
@simpanaja12 <simpanaja12@gmail.com>
2022-06-23 03:19:10 +0700

    Update kategori.php

--- app/application/views/dashboard/sub_belanja/kategori.php
+++ app/application/views/dashboard/sub_belanja/kategori.php
@@ -21,9 +21,10 @@
             <!-- /.container-fluid -->
             <div class="container-fluid">
                <div class="container">
-                  <?php foreach ($kategori as $k) { ?>
-                     <a class="btn btn-primary btn-lg" href="<?= base_url() . 'user/kategorilist/' . $k->id_kategorii ?>"><input type="hidden" name="id" value="<?= $k->id_kategorii ?>"><?= $k->nama_kategori ?></a>
-                  <?php } ?>
-               </div>
-            </div>
-            <!--akhir container-->
+                  <div class="row">
+                     <div class="col-4 ml-5 bg-appcard rounded">
+                        <?php foreach ($kategori as $k) { ?>
+                           <a class="btn btn-primary btn-lg" href="<?= base_url() . 'user/kategorilist/' . $k->id_kategorii ?>"><input type="hidden" name="id" value="<?= $k->id_kategorii ?>"><?= $k->nama_kategori ?></a>
+                        <?php } ?>
+                        <!--akhir container-->
+                     </div>
