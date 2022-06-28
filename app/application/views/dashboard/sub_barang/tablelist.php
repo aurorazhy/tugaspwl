cf243f9681d670a6909506e44da0bb16af9ff049
@simpanaja12 <simpanaja12@gmail.com>
2022-06-28 13:27:13 +0700

    Update tablelist.php

--- app/application/views/dashboard/sub_barang/tablelist.php
+++ app/application/views/dashboard/sub_barang/tablelist.php
@@ -1,56 +1,54 @@
-<div class="col-10 ml-5 p-0 bg-appcard rounded">
+<div class="container-fluid py-4">
+   <div class="col-11 ml-5 p-0 bg-appcard rounded">
 
-   <div class="card-body text-white bg-appregis">
-      <div class="row justify-content-between">
-         <div class="col-6">
-            <h5 class="card-title text-appgrey"> List Nama Barang </h5>
-         </div>
-         <div class="col-6 text-right">
-            <a href="<?= base_url() . 'user/stokbarang'; ?>" class="btn btn-appcard">+</a>
+      <div class="card-body text-white bg-appregis">
+         <div class="row justify-content-between">
+            <div class="col-6">
+               <h5 class="card-title text-appgrey"> List Nama Barang </h5>
+            </div>
+            <div class="col-6 text-right">
+               <a href="<?= base_url() . 'user/stokbarang'; ?>" class="btn btn-appcard">+</a>
+            </div>
          </div>
       </div>
-   </div>
-   <table class="table table-hover text-center">
-      <thead>
-         <tr>
-            <td> No </td>
-            <td> Nama Barang </td>
-            <td> Harga Beli </td>
-            <td> Qty </td>
-            <td> harga Jual </td>
-            <td> Nama Kategori </td>
-            <td></td>
-         </tr>
-      </thead>
-      <tbody>
-         <?php
-         $no = 1;
-         foreach ($barang as $b) {
-         ?>
+      <table class="table table-hover text-center">
+         <thead>
             <tr>
-               <td>
-                  <?= $no++; ?>
-                  <input type="hidden" name="id[]" value="<?= $b->id_barang  ?>">
-               </td>
-               <td width="200px"><?= $b->nama_barang ?></td>
-               <td><?= $b->harga_beli ?></td>
-               <td><?= $b->qty ?></td>
-               <td><?= $b->harga_jual ?></td>
-               <td> <?= $b->nama_kategori ?></td>
-               <td><a href="<?= base_url() . 'user/delbarang/' . $b->id_barang ?>" class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i></a></td>
+               <td> No </td>
+               <td> Nama Barang </td>
+               <td> Harga Beli </td>
+               <td> Qty </td>
+               <td> harga Jual </td>
+               <td> Nama Kategori </td>
+               <td></td>
             </tr>
-         <?php } ?>
-      </tbody>
-   </table>
-   </form>
-</div>
+         </thead>
+         <tbody>
+            <?php
+            $no = 1;
+            foreach ($barang as $b) {
+            ?>
+               <tr>
+                  <td>
+                     <?= $no++; ?>
+                     <input type="hidden" name="id[]" value="<?= $b->id_barang  ?>">
+                  </td>
+                  <td width="200px"><?= $b->nama_barang ?></td>
+                  <td><?= $b->harga_beli ?></td>
+                  <td><?= $b->qty ?></td>
+                  <td><?= $b->harga_jual ?></td>
+                  <td> <?= $b->nama_kategori ?></td>
+                  <td><a href="<?= base_url() . 'user/delbarang/' . $b->id_barang ?>" class="btn btn-outline-danger btn-sm"><i class="fas fa-trash"></i></a></td>
+               </tr>
+            <?php } ?>
+         </tbody>
+      </table>
+      </form>
+   </div>
 
 
 </div>
 
-
-</div>
-<!-- /.container-fluid -->
 
 </div>
 <!-- End of Main Content -->
