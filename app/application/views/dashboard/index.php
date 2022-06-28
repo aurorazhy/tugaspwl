b8b62be15846eb6fbb377f35df9e149a790d5858
@simpanaja12 <simpanaja12@gmail.com>
2022-06-28 16:27:14 +0700

    remove yang kurang jelas

--- app/application/views/dashboard/index.php
+++ app/application/views/dashboard/index.php
@@ -1,25 +1,6 @@
             <!-- Begin Page Content -->
-            <div class="container-fluid">
-
-               <!-- Topbar Search -->
-               <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                  <div class="input-group">
-                     <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button">
-                           <i class="fas fa-search fa-sm"></i>
-                        </button>
-                     </div>
-                  </div>
-               </form>
-
-               <!-- Page Heading
-               <h1 class="h3 mb-4 text-gray-800"> Blank Page </h1>-->
-
-
-            </div>
             <!-- /.container-fluid -->
-            <div class="container-fluid">
+            <div class="container-fluid mt-5">
                <div class="container">
                   <div class="row">
                      <div class="col-6 ml-4">
--- app/application/views/dashboard/sub_belanja/kategori.php
+++ app/application/views/dashboard/sub_belanja/kategori.php
@@ -1,30 +1,13 @@
             <!-- Begin Page Content -->
-            <div class="container-fluid">
-
-               <!-- Topbar Search -->
-               <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-                  <div class="input-group">
-                     <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                     <div class="input-group-append">
-                        <button class="btn btn-primary" type="button">
-                           <i class="fas fa-search fa-sm"></i>
-                        </button>
-                     </div>
-                  </div>
-               </form>
-
-               <!-- Page Heading
-               <h1 class="h3 mb-4 text-gray-800"> Blank Page </h1>-->
-
-
-            </div>
             <!-- /.container-fluid -->
-            <div class="container-fluid">
+            <div class="container-fluid mt-5">
                <div class="container">
                   <div class="row">
                      <div class="col-3 ml-5 bg-appcard rounded">
-                        <?php foreach ($kategori as $k) { ?>
-                           <a class="btn bg-info btn-lg m-1" href="<?= base_url() . 'user/kategorilist/' . $k->id_kategorii ?>"><input type="hidden" name="id" value="<?= $k->id_kategorii ?>"><?= $k->nama_kategori ?></a>
-                        <?php } ?>
+                        <div class="p-2 my-2">
+                           <?php foreach ($kategori as $k) { ?>
+                              <a class="btn bg-appside btn-lg m-1" href="<?= base_url() . 'user/kategorilist/' . $k->id_kategorii ?>"><input type="hidden" name="id" value="<?= $k->id_kategorii ?>"><?= $k->nama_kategori ?></a>
+                           <?php } ?>
+                        </div>
                         <!--akhir container-->
                      </div>
